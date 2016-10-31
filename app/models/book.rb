793a7ea23b0eb1e8dfdf7562c5d274a2ce337ff3
ngzikai <ngzikai92@gmail.com>
2016-10-31 20:05:29 +0800

    ZK's Commits
    Styling of homepage + Search Function

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,5 @@
 class Book < ApplicationRecord
+def self.search(search)
+  where("id LIKE ? OR title LIKE ? OR author_name LIKE ? or skill LIKE ?", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%") 
+end
 end
